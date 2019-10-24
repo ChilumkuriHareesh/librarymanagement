559f9be60565f36949a1c3ffbc9ca41c75eb7684
ChilumkuriHareesh <chilumkurihareesh@gmail.com>
2019-10-24 16:14:17 +0530

    Meta inf added and userdao implementation edited

--- src/main/java/com/capgemini/librarymanagement/controller/AdminRestController.java
+++ src/main/java/com/capgemini/librarymanagement/controller/AdminRestController.java
@@ -1,29 +1,28 @@
 package com.capgemini.librarymanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.capgemini.librarymanagement.beans.User;
 import com.capgemini.librarymanagement.service.AdminService;
+import com.capgemini.librarymanagement.service.AdminServiceImpl;
+import com.capgemini.librarymanagement.utility.UserResponse;
+
+import antlr.collections.List;
 
 public class AdminRestController {
-	@Autowired
-	private AdminService service;
 	
-	@PostMapping("/AdminLogin")
-	public UserResponse login(Integer id, String password) {
-		 UserResponse response=new UserResponse();
-		Users admin=service.adminLogin(id, password);
-		if(admin!=null) {
-			response.setStatusCode(201);
-			response.setMessage("success");
-			
-		}else {
-			response.setStatusCode(404);
-			response.setMessage("failed");
-			
-		}	return response;	
-	}
-
+	/*
+	 * @PostMapping("/AdminLogin") public UserResponse login(Integer id, String
+	 * password) { UserResponse response=new UserResponse(); Users
+	 * admin=service.adminLogin(id, password); if(admin!=null) {
+	 * response.setStatusCode(201); response.setMessage("success");
+	 * 
+	 * }else { response.setStatusCode(404); response.setMessage("failed");
+	 * 
+	 * } return response; }
+	 */
 
 	
-
+	
 }
